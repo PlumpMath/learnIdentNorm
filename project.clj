97f51c61568fc5f57fb01cf528e8e95f3e85714e
Thinh P. Tran <trphthinh@gmail.com>
2017-03-22 16:59:18 +0700

    Update libraries

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
 (defproject om-tutorial "0.1.0-SNAPSHOT"
   :description "My first Om program!"
-  :dependencies [[org.clojure/clojure "1.7.0"]
-                 [org.clojure/clojurescript "1.7.170"]
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [org.clojure/clojurescript "1.9.229"]
                  [org.omcljs/om "1.0.0-alpha21"]
                  [figwheel-sidecar "0.5.0-SNAPSHOT" :scope "test"]])
 
